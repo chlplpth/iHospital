9fd0d19b5365688834a504c617614cbc35634aa6
nn <melodiiz_noon@hotmail.com>
2015-11-17 13:39:32 +0700

    add appointment controller  except deleteAppointment

--- app/Http/Controllers/appointmentController.php
+++ app/Http/Controllers/appointmentController.php
@@ -9,34 +9,90 @@ use App\Http\Controllers\Controller;
 
 class appointmentController extends Controller
 {
-    //store to database
-    public function createAppointment(Request $request)
-    {
-    	$appointment = new appointment($request->all());
-    	Auth::user()->appointment()->save($article);
+    
 
-    	return redirect('appointment');
+    // user fill form and return available date for make appointment
+    public function createAppointmentRequest(Request $request)
+    {
+    	$date = $request->date;
+        $department = $request->departmentId;
+        $doctor = $request->doctorId;
+
+        if(doctor!=null)
+        {
+            $availableDate = App\schedule::where('doctorId',$doctor);
+                                         ->where('date',$date);
+                                         ->get();
+        }
+        else
+        {
+            $availableDate = App\schedule::where('departmentId',$department);
+                                         ->where('date',$date);
+                                         ->get();
+        }
+    	
+        return view(appointment.availableDate,compact($availableDate));
+    }   
+
+    // show everything user fill in confirmation page
+    public function createAppointmentConfirm(Request $request)
+    {
+
+        return view('appointment.confirm',compact($request));
+    }
+
+    //user enter confirm and store data of appointment in database
+    public function createAppointmentStore(Request $request)
+    {
+        $appointment = new appointment($request->all());
+        Auth::user()->appointment()->save($article);
+
+        return redirect('appointment');
     }
 
     public function viewAppointmentPatient(Request $request)
     {
         $patient = $request->patientId;
 
-        $appointments = App\appointment::where('patentId', $patient)
+        $appointments = App\appointment::where('patientId', $patient)
                //->orderBy('name', 'desc')
                //->take(10)
                ->get();
 
+        return view(appointment.all,compact(appointments);
     }
 
-    public function delayAppointment(Request $request)
+    public function delayAppointmentRequest(Request $request)
+    {
+        $appointment = App\Appointment::find($request->appointmentId);
+        $date = $appointment->date;
+        $department = $appointment->departmentId;
+        $doctor = $appointment->doctorId;
+
+        if($doctor!=null)
+        {
+            $availableDate = App\schedule::where('doctorId',$doctor);
+                                         ->where('date',$date);
+                                         ->get();
+        }
+        else
+        {
+            $availableDate = App\schedule::where('departmentId',$department);
+                                         ->where('date',$date);
+                                         ->get();
+        }
+        
+        return view(appointment.availableDate,compact($availableDate));
+    }
+
+    public function delayAppointmentStore(Request $request)
     {
         $appointment = App\Appointment::find($request->appointmentId);
         $appointment->date = $request->date;
         $appointment->time = $request->time;
         $appointment->doctorId = $request->doctorId;
         $appointment->save();
-        
+        return redirect('appointment');
     }
 
     public function cancelAppointment(Request $request)
--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -9,5 +9,47 @@ use App\Http\Controllers\Controller;
 
 class userController extends Controller
 {
-    //
+   
+    public function registerPatient(Request $request)
+    {
+    	$idNo = $request->idNo;
+    	
+    }
+    public function viewProfile(Request $request)
+    {
+    		
+    }
+    public function viewPatientProfile(Request $request)
+    {
+    	$patient = App/patient::find($request->userId);
+    	return view(patient.profile,compact($patient));	
+    }
+    public function editPatientProfile(Request $request)
+    {
+    		
+    }
+    public function getDoctorList(Request $request)
+    {
+    		
+    }
+    public function searchDoctor(Request $request)
+    {
+    		
+    }
+    public function searchPatient(Request $request)
+    {
+    		
+    }
+    public function viewDoctorProfile(Request $request)
+    {
+    		
+    }
+    public function addPatient(Request $request)
+    {
+    		
+    }
+    public function addHospitalStaff(Request $request)
+    {
+    		
+    }
 }
