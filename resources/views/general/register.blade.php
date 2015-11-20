6f9d2fc1213ca0712d9b50151e339e6d458b9d79
Kornosk130 <kornosk130@gmail.com>
2015-11-20 19:50:34 +0700

    Solve datepicker
    register
    createAppointmentForPatient
    createAppointment

--- resources/views/general/register.blade.php
+++ resources/views/general/register.blade.php
@@ -46,10 +46,15 @@
 					<div class ="col-md-2"><br>
 						{!!Form::label('bloodGroup', 'กรุ๊ปเลือด');!!}
 						{!!Form::select('bloodGroup', array('A' => 'A', 'B' => 'B', 'O' => 'O', 'AB' => 'AB'));!!}
-					</div>
-					<div class ="col-md-7 birthDate"><br>
-						{!!Form::label('dateOfBirth', 'วัน/เดือน/ปี เกิด');!!}
-						{!!Form::text('dateOfBirth','',['class'=>'textbox','placeholder'=>'วว/ดด/ปป']);!!}
+					</div><br>
+					<div class ="col-md-5 form-inline">
+						{!!Form::label('dateOfBirth', 'วัน/เดือน/ปี เกิด')!!} &nbsp
+						<div class="input-group date" id="datepicker">
+							{!! Form::text('dateOfBirth', '', ['class' => 'form-control', 'placeholder'=>'วว/ดด/ปป']) !!}
+							<div class="input-group-addon">
+								<span class="glyphicon glyphicon-calendar"></span>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -169,7 +174,13 @@
 		</div>
 		{!!Form::submit('ลงทะเบียน',['class'=>'btn btn-success register']);!!}	
 	</div>
-
+	<!-- Script to construct datepicker -->
+	<script type="text/javascript">
+	    // When the document is ready
+	    $(document).ready(function () {
+	    	$('#datepicker').datepicker();
+    	});
+    </script>
 	
 </div>
 {!! Form::close() !!}
