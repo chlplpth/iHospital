dd9e2a1bcd548c13bd0d92c438f760bcf6a3b05d
Milotic <poke_pokemon1@hotmail.com>
2015-11-23 06:19:01 +0700

    finish pdf and email controller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -25,7 +25,7 @@ Route::get('/register', function () {
 Route::get('/changePassword', function () {
     return view('general/changePassword');
 });
-    
+
 Route::get('/forgetPassword', function () {
     return view('general/forgetPassword');
 });
@@ -169,7 +169,7 @@ Route::get('/diagnose', function () {
 Route::get('sendemail', function () {
     $data = array(
         'name' => "Noon",
-    );
+        );
     
 
     Mail::send('emails.createStaffEmail',$data,function ($message) {
@@ -213,13 +213,121 @@ Route::post('/register', 'Auth\AuthController@register');
 Route::get('/genPassword/{text}', 'Auth\AuthController@genPassword');
 Route::get('/testModel', 'Auth\AuthController@testModel');
 
+Route::get('/pdf', function(){
+    $data = [
+    'month' => 'มกราคม',
+    'date' => '21/11/58',
+    'doctorName' => 'เทรุ',
+    'department' => 'ทำคลอด',
+    'diag' => ['1'=>['diagDate'=>'1/1/2558','morning'=>'3','afternoon'=>'12'],
+    '2'=>['diagDate'=>'2/1/2558','morning'=>'1','afternoon'=>'7'],
+    '3'=>['diagDate'=>'3/1/2558','morning'=>'5','afternoon'=>'2'],
+    '4'=>['diagDate'=>'4/1/2558','morning'=>'1','afternoon'=>'7'],
+    '5'=>['diagDate'=>'5/1/2558','morning'=>'1','afternoon'=>'7'],
+    '6'=>['diagDate'=>'6/1/2558','morning'=>'1','afternoon'=>'7'],
+    '7'=>['diagDate'=>'7/1/2558','morning'=>'1','afternoon'=>'7'],
+    '8'=>['diagDate'=>'8/1/2558','morning'=>'1','afternoon'=>'7'],
+    '9'=>['diagDate'=>'9/1/2558','morning'=>'1','afternoon'=>'7'],
+    '10'=>['diagDate'=>'10/1/2558','morning'=>'1','afternoon'=>'7'],
+    '11'=>['diagDate'=>'11/1/2558','morning'=>'1','afternoon'=>'7'],
+    '12'=>['diagDate'=>'12/1/2558','morning'=>'1','afternoon'=>'7'],
+    '13'=>['diagDate'=>'13/1/2558','morning'=>'1','afternoon'=>'7'],
+    '14'=>['diagDate'=>'14/1/2558','morning'=>'1','afternoon'=>'7'],
+    '15'=>['diagDate'=>'15/1/2558','morning'=>'1','afternoon'=>'7'],
+    '16'=>['diagDate'=>'16/1/2558','morning'=>'1','afternoon'=>'7'],
+    '17'=>['diagDate'=>'17/1/2558','morning'=>'1','afternoon'=>'7'],
+    '18'=>['diagDate'=>'18/1/2558','morning'=>'5','afternoon'=>'2'],
+    '19'=>['diagDate'=>'19/1/2558','morning'=>'1','afternoon'=>'7'],
+    '20'=>['diagDate'=>'20/1/2558','morning'=>'1','afternoon'=>'7'],
+    '21'=>['diagDate'=>'21/1/2558','morning'=>'1','afternoon'=>'7'],
+    '22'=>['diagDate'=>'22/1/2558','morning'=>'1','afternoon'=>'7'],
+    '23'=>['diagDate'=>'23/1/2558','morning'=>'1','afternoon'=>'7'],
+    '24'=>['diagDate'=>'24/1/2558','morning'=>'1','afternoon'=>'7'],
+    '25'=>['diagDate'=>'25/1/2558','morning'=>'1','afternoon'=>'7'],
+    '26'=>['diagDate'=>'26/1/2558','morning'=>'1','afternoon'=>'7'],
+    '27'=>['diagDate'=>'27/1/2558','morning'=>'1','afternoon'=>'7'],
+    '28'=>['diagDate'=>'28/1/2558','morning'=>'1','afternoon'=>'7'],
+    '29'=>['diagDate'=>'29/1/2558','morning'=>'1','afternoon'=>'7'],
+    '30'=>['diagDate'=>'30/1/2558','morning'=>'1','afternoon'=>'7'],
+    '31'=>['diagDate'=>'31/1/2558','morning'=>'1','afternoon'=>'7'],
+    ] 
+    ];
+    $pdf=PDF::loadView('doctor/showDiagnosisHistoryPdf',$data)->setOption('page-size', 'A4');
+    return $pdf->stream();
+});
 
+Route::get('/diagPdf', function(){
+
+    $data = [
+    'date'         => '21/11/58',
+    'HN'    => 'HN-00000000',
+    'patName'      => 'ผู้ป่วย',
+    'patLast'         => 'น๊ะจ๊ะ',
+    'sex'      => 'ชาย',
+    'bloodType'         => 'O',
+    'age'         => '21',
+    'doctorName'    => 'เทรุ',
+    'department'      => 'ทำคลอด',
+    'diagDate'         => '21/11/58',
+    'diagTime'      =>'9.00-12.00 น.',
+    'symptom'      => 'ท้อง',
+    'diag'         => 'ท้องๆๆๆๆๆ',
+    'medicine'  => ['1'=>['drugName'=>'para','quantity'=>'5'],
+    '2'=>['drugName'=>'para2','quantity'=>'10']
+    ]
+    ];
+    $pdf=PDF::loadView('patient/diagRecordPdf',$data)->setOption('page-size', 'A4');
+    return $pdf->stream();
+});
+Route::get('/test', function () {
+    $data = [
+    'month' => 'มกราคม',
+    'date' => '21/11/58',
+    'doctorName' => 'เทรุ',
+    'department' => 'ทำคลอด',
+    'diag' => ['1'=>['diagDate'=>'1/1/2558','morning'=>'3','afternoon'=>'12'],
+    '2'=>['diagDate'=>'2/1/2558','morning'=>'1','afternoon'=>'7'],
+    '3'=>['diagDate'=>'3/1/2558','morning'=>'5','afternoon'=>'2'],
+    '4'=>['diagDate'=>'4/1/2558','morning'=>'1','afternoon'=>'7'],
+    '5'=>['diagDate'=>'5/1/2558','morning'=>'1','afternoon'=>'7'],
+    '6'=>['diagDate'=>'6/1/2558','morning'=>'1','afternoon'=>'7'],
+    '7'=>['diagDate'=>'7/1/2558','morning'=>'1','afternoon'=>'7'],
+    '8'=>['diagDate'=>'8/1/2558','morning'=>'1','afternoon'=>'7'],
+    '9'=>['diagDate'=>'9/1/2558','morning'=>'1','afternoon'=>'7'],
+    '10'=>['diagDate'=>'10/1/2558','morning'=>'1','afternoon'=>'7'],
+    '11'=>['diagDate'=>'11/1/2558','morning'=>'1','afternoon'=>'7'],
+    '12'=>['diagDate'=>'12/1/2558','morning'=>'1','afternoon'=>'7'],
+    '13'=>['diagDate'=>'13/1/2558','morning'=>'1','afternoon'=>'7'],
+    '14'=>['diagDate'=>'14/1/2558','morning'=>'1','afternoon'=>'7'],
+    '15'=>['diagDate'=>'15/1/2558','morning'=>'1','afternoon'=>'7'],
+    '16'=>['diagDate'=>'16/1/2558','morning'=>'1','afternoon'=>'7'],
+    '17'=>['diagDate'=>'17/1/2558','morning'=>'1','afternoon'=>'7'],
+    '18'=>['diagDate'=>'18/1/2558','morning'=>'5','afternoon'=>'2'],
+    '19'=>['diagDate'=>'19/1/2558','morning'=>'1','afternoon'=>'7'],
+    '20'=>['diagDate'=>'20/1/2558','morning'=>'1','afternoon'=>'7'],
+    '21'=>['diagDate'=>'21/1/2558','morning'=>'1','afternoon'=>'7'],
+    '22'=>['diagDate'=>'22/1/2558','morning'=>'1','afternoon'=>'7'],
+    '23'=>['diagDate'=>'23/1/2558','morning'=>'1','afternoon'=>'7'],
+    '24'=>['diagDate'=>'24/1/2558','morning'=>'1','afternoon'=>'7'],
+    '25'=>['diagDate'=>'25/1/2558','morning'=>'1','afternoon'=>'7'],
+    '26'=>['diagDate'=>'26/1/2558','morning'=>'1','afternoon'=>'7'],
+    '27'=>['diagDate'=>'27/1/2558','morning'=>'1','afternoon'=>'7'],
+    '28'=>['diagDate'=>'28/1/2558','morning'=>'1','afternoon'=>'7'],
+    '29'=>['diagDate'=>'29/1/2558','morning'=>'1','afternoon'=>'7'],
+    '30'=>['diagDate'=>'30/1/2558','morning'=>'1','afternoon'=>'7'],
+    '31'=>['diagDate'=>'31/1/2558','morning'=>'1','afternoon'=>'7'],
+    ] 
+    ];
+    return view('doctor/showDiagnosisHistoryPdf',$data);
+});
 // ================= FOR SMURF CONTROLLER (VALIDATION) ============================
 Route::post('/editProfile', 'SmurfController@editProfileValidate');
 
-
-
-
+Route::get('/forgetPasswordEmail', 'emailController@forgetPasswordEmail');
+Route::get('/postponedAppointmentEmail', 'emailController@postponedAppointmentEmail');
+Route::get('/createStaffEmail', 'emailController@createStaffEmail');
+Route::get('/confirmRegistrationEmail', 'emailController@confirmRegistrationEmail');
+Route::get('/confirmAppointmentEmail', 'emailController@confirmAppointmentEmail');
 
 Route::post('/importSchedule', 'scheduleController@importSchedule');
 Route::post('/addDepartment', 'departmentController@addDepartment');
