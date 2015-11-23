e39f574447be676a8a08cc42f73bacc85074b12f
Kornosk130 <kornosk130@gmail.com>
2015-11-24 05:56:04 +0700

    fix

--- resources/views/staff/manageAppointmentForPatient.blade.php
+++ resources/views/staff/manageAppointmentForPatient.blade.php
@@ -3,34 +3,13 @@
 <link href="{{asset('css/staff.css')}}" rel="stylesheet">
 @stop
 @section('content')
-{!! Form::open(array('url' => 'foo/bar')) !!}
 
 <div class="panel panel-default">
 	<div class="panel-heading">
-		<h3 class="panel-title">จัดการการนัดหมาย</h3>
+		<h3 class="panel-title">ตารางการนัดหมาย</h3>
 	</div>
-	<div class="panel-body" style="margin-top:2%; margin-left:40px;">
-		<form role="form">
-			<div class="form-group row">
-				<div class="col-xs-2">{!! Form::label('name', 'ชื่อ / รหัสผู้ป่วย'); !!}</div>
-				<div class="col-xs-3" >{!! Form::text('name', '', ["class" => "form-control", 'placeholder'=>'ชื่อ / รหัสผู้ป่วย']) !!}</div>
-				<div class="col-xs-6" >{!! Form::submit('ค้นหา', ["class" => "btn btn-info"]) !!}</div>
-			</div>
-			<div class="form-group row">
-				<div class="col-xs-2">{!! Form::label('id', 'รหัสผู้ป่วย'); !!}</div>
-				<div class="col-xs-2">{!! Form::label('id', '12345678'); !!}</div>
-			</div>
-			<div class="form-group row">
-				<div class="col-xs-2">{!! Form::label('name', 'ชื่อ'); !!}</div>
-				<div class="col-xs-2">{!! Form::label('name', 'ชลัมพล'); !!}</div>
-			</div>
-			<div class=" form-group row">
-				<div class="col-xs-2">{!! Form::label('lastname', 'นามสกุล'); !!}</div>
-				<div class="col-xs-2">{!! Form::label('lastname', 'ไก๊ไก่ไก๊ไก่'); !!}</div>
-			</div><br>
-			<div class="form-group row">
-				<div class="col-xs-2">{!! Form::label('appointmentList', 'รายการนัดหมาย'); !!}</div>
-			</div>
+	<div class="panel-body">
+		<form>
 			<div class="form-group row">
 				<div class="col-xs-1"></div>
 				<div class="col-xs-10">
@@ -48,12 +27,12 @@
 						<tbody>
 							@foreach($appointments as $app)
 							<tr>
-								<td>{{ 20/11/2558 }}</td>
+								<td>{{ $app->diagDate }}</td>
 								<td>{{ $app->diagTime }}</td>
 								<td>{{ $app->departmentName }}</td>
 								<td>{{ $app->name }} {{ $app->surname }}</td>
-								<td ><a href="{{ url('/rescheduleAppointmentByStaff') }}" class="btn btn-warning">เลื่อน</a></td>
-								<td ><button class="btn btn-danger">ยกเลิก</button></td>
+								<td ><a href="{{ url('/rescheduleAppointmentByStaff/' . $app->appointmentId ) }}" class="btn btn-warning">เลื่อน</a></td>
+								<td ><a href="{{ url('/cancelAppointmentByStaff/' . $app->appointmentId ) }}" class="btn btn-danger">ยกเลิก</a></td>
 							</tr>
 							@endforeach
 						</tbody>
@@ -64,14 +43,4 @@
 		</form>
 	</div>
 </div>
-
-<script type="text/javascript">
-	$(document).ready(function () {
-		$('.btn-danger').click(function() {
-			swal({   title: "ต้องการลบการนัดหมายนี้หรือไม่",   text: "หากยืนยันรการนัดหมายนี้จะถูกลบทันที",   type: "warning",   showCancelButton: true,   confirmButtonColor: "#DD6B55",   confirmButtonText: "ยืนยัน",   closeOnConfirm: true });
-		});
-	});
-</script>
-
-{!! Form::close() !!}
 @stop
