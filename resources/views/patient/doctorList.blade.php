ae7706170f49a54fea6514fde6b8d510ef705dba
Milotic <poke_pokemon1@hotmail.com>
2015-11-24 02:36:20 +0700

    fix doctorList view

--- resources/views/patient/doctorList.blade.php
+++ resources/views/patient/doctorList.blade.php
@@ -10,73 +10,94 @@
 	<div class="panel-heading">
 		<h3 class="panel-title">ข้อมูลแพทย์</h3>
 	</div>
-	<br><br>
-	<div class="row">
-		<div class="col-md-1"></div>
-		<div class="col-md-4 form-inline">{!! Form::label('department', 'แผนก'); !!} &nbsp
-
-			{!! Form::select('department', array(
-			'0' => 'ไม่ระบุ',
-			'1' => 'กายวิภาคศาสตร์', 
-			'2' => 'กุมารเวชศาสตร์',
-			'3' => 'จิตเวชศาสตร์',
-			'4' => 'จุลชีววิทยา',
-			'5' => 'จักษุวิทยา',
-			'6' => 'ชีวเคมี',
-			'7' => 'นิติเวชศาสตร์',
-			'8' => 'ปรสิตวิทยา',
-			'9' => 'พยาธิวิทยา',
-			'10' => 'เภสัชวิทยา',
-			'11' => 'รังสีวิทยา',
-			'12' => 'วิสัญญีวิทยา',
-			'13' => 'เวชศาสตร์ชันสูตร',
-			'14' => 'เวชศาสตร์ป้องกันและสังคม',
-			'15' => 'เวชศาสตร์ฟื้นฟู',
-			'16' => 'ศัลยศาสตร์',
-			'17' => 'สรีรวิทยา',
-			'18' => 'สุติศาสตร์-นารีเวชวิทยา',
-			'19' => 'โสต คอ นาสิกวิทยา',
-			'20' => 'ออโธปิดิกส์',
-			'21' => 'อายุรศาสตร์'),'0',["class" => "form-control"])!!}
-			@if( $errors->has('department') )<br><br>
-          			<p class="text-danger"> {{ $errors->first('department') }} </p>
-        	@endif</div>
-
-			
-			<div class="col-md-4">{!! Form::label('name', 'ชื่อแพทย์'); !!} &nbsp
-				{!! Form::text('doctor', '', ['class'=>'textbox', 'placeholder'=>'ณภัทร']);!!}
-				@if( $errors->has('name') )<br><br>
-          			<p class="text-danger"> {{ $errors->first('name') }} </p> 
-        	@endif
-			</div>
-			
-			<div class="col-md-3">{!! Form::submit('ค้นหา', ["class" => "btn btn-defalut", "id" => "searchButton"]) !!}
-			</div>
-
-		</div>
-		
-		<br><br>
-		
-
+	<div class="panel-body">
+		<div id="doctorList">
 			<div class="row">
-				
-				<div class="col-md-3">
-					<a href="#" class="thumbnail">
-						{!! HTML::image('image/DoctorPicture/smurf.jpg', 'Profile', ['width' => '200px' , 'height' => '200px' ]) !!}
-					</a>
+				<div class="col-md-1"></div>
+				<div class="col-md-4 form-inline">{!! Form::label('department', 'แผนก'); !!} &nbsp
+
+					{!! Form::select('department', array(
+					'0' => 'ไม่ระบุ',
+					'1' => 'กายวิภาคศาสตร์', 
+					'2' => 'กุมารเวชศาสตร์',
+					'3' => 'จิตเวชศาสตร์',
+					'4' => 'จุลชีววิทยา',
+					'5' => 'จักษุวิทยา',
+					'6' => 'ชีวเคมี',
+					'7' => 'นิติเวชศาสตร์',
+					'8' => 'ปรสิตวิทยา',
+					'9' => 'พยาธิวิทยา',
+					'10' => 'เภสัชวิทยา',
+					'11' => 'รังสีวิทยา',
+					'12' => 'วิสัญญีวิทยา',
+					'13' => 'เวชศาสตร์ชันสูตร',
+					'14' => 'เวชศาสตร์ป้องกันและสังคม',
+					'15' => 'เวชศาสตร์ฟื้นฟู',
+					'16' => 'ศัลยศาสตร์',
+					'17' => 'สรีรวิทยา',
+					'18' => 'สุติศาสตร์-นารีเวชวิทยา',
+					'19' => 'โสต คอ นาสิกวิทยา',
+					'20' => 'ออโธปิดิกส์',
+					'21' => 'อายุรศาสตร์'),'0',["class" => "form-control"])!!}
+					@if( $errors->has('department') )<br><br>
+					<p class="text-danger"> {{ $errors->first('department') }} </p>
+					@endif
 				</div>
-				<div class="col-md-6">
-					<span class ="header">ชื่อแพทย์ : </span> <br><br>
-					<span class ="header">แผนก : </span> <br><br>
-					<span class ="header">ความเชี่ยวชาญ : </span><br><br>
-					<span class ="header">ประวัติการศึกษา : </span>Chulalongkorn University 
+
+
+				<div class="col-md-4">{!! Form::label('name', 'ชื่อแพทย์'); !!} &nbsp
+					{!! Form::text('doctor', '', ['class'=>'textbox', 'placeholder'=>'ณภัทร']);!!}
+					@if( $errors->has('name') )<br><br>
+					<p class="text-danger"> {{ $errors->first('name') }} </p> 
+					@endif
+				</div>
+
+				<div class="col-md-3">{!! Form::submit('ค้นหา', ['class' => 'btn btn-info', 'id' => 'searchDoctorButton']) !!}</div>
+			</div>
+			<br><br>
+			<div id="doctorData">
+				<div class="row">	
+					<div class="col-md-1"></div>
+					<div class="col-md-3">
+						{!! HTML::image('image/DoctorPicture/smurf.jpg', 'Profile', ['width' => '200px' , 'height' => '200px' ]) !!}
+					</div>
+					<div class="col-md-6"><br>
+						<span class ="header">ชื่อแพทย์ : </span> <br><br>
+						<span class ="header">แผนก : </span> <br><br>
+						<span class ="header">ความเชี่ยวชาญและประสบการณ์ : </span><br><br>
+					</div>
+					<div class="col-md-2"></div>
+				</div>
+				<br>
+				<div class="row">	
+					<div class="col-md-1"></div>
+					<div class="col-md-3">
+						{!! HTML::image('image/DoctorPicture/smurf.jpg', 'Profile', ['width' => '200px' , 'height' => '200px' ]) !!}
+					</div>
+					<div class="col-md-6"><br>
+						<span class ="header">ชื่อแพทย์ : </span> <br><br>
+						<span class ="header">แผนก : </span> <br><br>
+						<span class ="header">ความเชี่ยวชาญและประสบการณ์ : </span><br><br>
+					</div>
+					<div class="col-md-2"></div>
+				</div>
+				<br>
+				<div class="row">	
+					<div class="col-md-1"></div>
+					<div class="col-md-3">
+						{!! HTML::image('image/DoctorPicture/smurf.jpg', 'Profile', ['width' => '200px' , 'height' => '200px' ]) !!}
+					</div>
+					<div class="col-md-6"><br>
+						<span class ="header">ชื่อแพทย์ : </span> <br><br>
+						<span class ="header">แผนก : </span> <br><br>
+						<span class ="header">ความเชี่ยวชาญและประสบการณ์ : </span><br><br>
+					<div class="col-md-2"></div>
 				</div>
 			</div>
-		
-		
-		
-
+		</div>
 	</div>
+
+</div>
 {!! Form::close() !!}
 
-	@stop
+@stop
