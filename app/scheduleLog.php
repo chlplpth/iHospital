592210848ee69ab2d2144bf1b8fd430ff05e99df
Kornosk130 <kornosk130@gmail.com>
2015-12-13 14:09:10 +0700

    for pull

--- app/scheduleLog.php
+++ app/scheduleLog.php
@@ -37,10 +37,28 @@ class scheduleLog extends Model
 
     //---------------------------------  function ---------------------------------
 
+    public static function newInstantScheduleLog($diagDate, $doctorId)
+    {
+        $slog = new scheduleLog;
+        $slog->doctorId = $doctorId;
+        $slog->startDate = $diagDate;
+        $slog->endDate = $diagDate;
+        $slog->diagDateList = '00000000000000';
+        $slog->save();
+        return $slog;
+    }
+
     public static function importSchedule($request)
     {
-        $morning = scheduleLog::getDiagDateString($request['m']);
-        $afternoon = scheduleLog::getDiagDateString($request['a']);
+        $morning = $afternoon = '0000000';
+        if(isset($request['m']))
+        {
+            $morning = scheduleLog::getDiagDateString($request['m']);
+        }
+        if(isset($request['a']))
+        {
+            $afternoon = scheduleLog::getDiagDateString($request['a']);
+        }
         $request['diagDateList'] = $morning . $afternoon;
 
         $request['startDate'] = scheduleLog::changeDateFormat($request['startDate']);
